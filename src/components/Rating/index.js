93bcffbe393f8c19d669b40b82d51542e2469a7c
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2018-11-01 18:25:19 +0100

    Again! Fix passing props in constructor

--- src/components/Rating/index.js
+++ src/components/Rating/index.js
@@ -13,10 +13,10 @@ const ratingDesc = {
 };
 
 class Rating extends Component {
-	constructor(rating) {
-		super(rating);
+	constructor(props) {
+		super(props);
 		this.state = {
-			value: rating,
+			value: props.rating,
 		}
 	}
 
@@ -29,13 +29,13 @@ class Rating extends Component {
 		this.setState({value: rate});
 	}
 
-	// handleMouseEnter = (rate) => {
-	// 	this.setState({value: rate});
-	// }
+	handleMouseEnter = (rate) => {
+		this.setState({value: rate});
+	}
 
-	// handleMouseLeave = (previousRate) => {
-	// 	this.setState({value: previousRate});
-	// }
+	handleMouseLeave = (previousRate) => {
+		this.setState({value: previousRate});
+	}
 
 
 	render(){
@@ -46,8 +46,8 @@ class Rating extends Component {
 							<FontAwesomeIcon icon={faStar} id={starKey}
 							className={ starKey <=this.state.value ? styles.starFill : styles.star}
 							onClick={()=>this.handleClick(starKey)}
-							// onMouseEnter={()=> this.handleMouseEnter(starKey)}
-							// onMouseLeave={()=>this.handleMouseLeave(previousRate)}
+							onMouseEnter={()=> this.handleMouseEnter(starKey)}
+							onMouseLeave={()=>this.handleMouseLeave(previousRate)}
 							/>
 					))
 				}
