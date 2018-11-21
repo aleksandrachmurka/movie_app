2b99061258966f6e8407df2677772f9c0b5a4883
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2018-11-21 20:16:17 +0100

    Update test to Rating Component

--- src/components/spare_components/Rating/Rating.test.js
+++ src/components/spare_components/Rating/Rating.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Rating from './index.js';
 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
 describe('Rating', () =>  {
 	const setup = () => {
 		const wrapper = mount(<Rating />);
@@ -21,12 +25,15 @@ describe('Rating', () =>  {
 		const { wrapper, description, star } = setup();
 
 		it('should show \'Brak oceny\' when there is no rating', () => {
+			console.log(wrapper.state());
 			expect(description()).text().toBe('Brak oceny');
 		});
 
 		it('should show correct rating description', () => {
+			console.log(wrapper.state());
 			star().at(2).simulate('click');
 			expect(description()).text().toBe('Zjadliwy');
+			console.log(wrapper.state());
 		});
 	});
 
@@ -34,14 +41,19 @@ describe('Rating', () =>  {
 		const { wrapper, description, star, fillledStar } = setup();
 
 		it('should set rating when clicked', () =>{
+			console.log(wrapper.state());
 			star().at(1).simulate('click');
 			expect(fillledStar()).toHaveLength(2);
+			console.log(wrapper.state());
 		});
 
 		it('should set rating to 0 when clicked twice', () => {
+			console.log(wrapper.state());
 			star().at(2).simulate('click');
+			expect(fillledStar()).toHaveLength(3);
 			star().at(2).simulate('click');
 			expect(fillledStar()).toHaveLength(0);
+			console.log(wrapper.state());
 		});
 
 		it('should set rating when hovered', () => {
